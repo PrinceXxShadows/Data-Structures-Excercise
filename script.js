8b6de574a6e26f20365b72da89e9a37f516f02a6
“PrinceXxShadows” <princexxshadows@gmail.com>
2022-03-10 09:12:39 -0600

    Excercise complete

---
+++ script.js
@@ -0,0 +1,19 @@
+const jedi = [];
+
+jedi[0] = `Luke`;
+console.log(jedi)
+
+jedi.push(`Obi-Wan Kenobi`);
+console.log(jedi);
+
+jedi.unshift(`Yoda`);
+console.log(jedi)
+
+console.log(jedi[1])
+
+const force = jedi.pop();
+console.log(jedi[2]);
+
+jedi.shift();
+console.log(jedi);
+
